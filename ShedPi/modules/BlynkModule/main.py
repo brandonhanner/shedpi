c717fc8b353540a9bf0ed74b6333ac9169566ffd
brandonhanner <bhanner95@gmail.com>
2020-06-19 17:34:59 -0500

    init commit

---
+++ ShedPi/modules/BlynkModule/main.py
@@ -0,0 +1,91 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for
+# full license information.
+
+import time
+import os
+import sys
+from azure.iot.device.aio import IoTHubModuleClient
+
+import BlynkLib
+
+from pymodbus.client.asynchronous.serial import (AsyncModbusSerialClient as ModbusClient)
+from pymodbus.client.asynchronous import schedulers
+
+import Adafruit_DHT
+
+from w1thermsensor import W1ThermSensor
+
+from gpiozero import LED
+
+import board
+import busio
+
+i2c = busio.I2C(board.SCL, board.SDA)
+
+import adafruit_ads1x15.ads1115 as ADS
+
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
+class ADC(object):
+    def __init__(self):
+        self.ads = ADS.ADS1115(i2c)
+        self.ads.gain = 1
+        self.chan = AnalogIn(self.ads, ADS.P0)
+
+        self.s0 = LED(19)
+        self.s1 = LED(13)
+        self.s2 = LED(6)
+        self.s3 = LED(5)
+
+        self.mux_en = LED(26)
+
+        self.mux_en.off()
+
+        self.bcdPins = [self.s0,self.s1,self.s2,self.s3]
+
+        #S3 -> GPIO5
+        #S2 -> GPIO6
+        #S1 -> GPIO13
+        #S0 -> GPIO19 lsb
+        #EN -> GPIO26
+
+        #CELL4 -> C15
+        #CELL3 -> C14
+        #CELL2 -> C13
+        #CELL1 -> C12
+
+        #CURRENT1 -> C1
+        #CURRENT2 -> C0
+
+        #UART -> UART
+
+        #ADS -> I2C
+    def capture_readings(self):
+        for i in [0,1,12,13,14,15]:
+            volts = self.read_channel(i)
+            print("Channel: {} Voltage: {}".format(i,volts))
+            
+    def read_channel(self, channel):
+        self.set_channel(channel)
+        time.sleep(.05)
+        return self.chan.voltage
+
+
+    def set_channel(self, channel):
+        for j in range(0,4):
+                digit = (channel & (1 << j)) >> j
+                # print("{} element: {}".format(j,digit))
+                if digit == 1:
+                    self.bcdPins[j].on()
+                else:
+                    self.bcdPins[j].off()
+
+adc = ADC()
+
+while True:
+
+    adc.capture_readings()
+    
+    time.sleep(1)
